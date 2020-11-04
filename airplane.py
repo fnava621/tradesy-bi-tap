f74b4dfaed590c2567d8cd4f27b6b2644d466f0e
Fernando Nava <fnava621@gmail.com>
2020-11-04 13:57:49 -0800

    save

--- airplane.py
+++ airplane.py
@@ -1,6 +1,19 @@
 class Airplane:
-    def __init__(self):
+    def __init__(self, type):
         print("A new instance got made!")
+        self.type = type
 
     def fly(self):
-        print("I'm flying!")
+        if self.type == "Boeing 747":
+            print("Flying like boss")
+        elif self.type == "Cessna":
+            print("Flying like an eagle!!")
+        else:
+            print("I'm flying")
+
+
+boeing_747 = Airplane('Boeing 747')
+boeing_747.fly()
+
+cessna = Airplane('Cessna')
+cessna.fly()
